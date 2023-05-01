5caa8be155a1e1581917dd887e24e59c0f4c8e17
semon69 <smdemon3@gmail.com>
2023-05-01 22:02:08 +0600

    add data set

---
+++ data/chefs.jsx
@@ -0,0 +1,346 @@
+[{
+    "id": 1, "picture": "https://example.com/chef1.jpg", "name": "Julia Child", "experience": 50, "recipes": ["Boeuf Bourguignon", "Coq au Vin", "Salade Niçoise"],
+    "recipes": [
+        {
+          "name": "Boeuf Bourguignon",
+          "ingredients": [
+            "3 lbs beef chuck roast",
+            "6 slices of bacon",
+            "1 bottle of red wine",
+            "1 onion, chopped",
+            "2 garlic cloves, minced",
+            "3 cups beef broth",
+            "3 carrots, peeled and sliced",
+            "1 bouquet garni (thyme, bay leaves, parsley)"
+          ],
+          "method": "Preheat the oven to 350°F. Cook bacon in a Dutch oven until crisp. Remove the bacon and set aside. Add beef to the Dutch oven and brown on all sides. Remove beef and set aside. Add onion and garlic to Dutch oven and cook until softened. Add wine and beef broth, and bring to a simmer. Add the beef, bacon, carrots, and bouquet garni to the Dutch oven. Cover and cook in the oven for 2-3 hours or until the beef is tender. Remove the bouquet garni and serve.",
+          "rating": 4.5
+        },
+        {
+          "name": "Coq au Vin",
+          "ingredients": [
+            "4 chicken legs",
+            "4 slices of bacon",
+            "1 onion, chopped",
+            "2 garlic cloves, minced",
+            "2 cups red wine",
+            "2 cups chicken broth",
+            "2 tbsp tomato paste",
+            "1 bouquet garni (thyme, bay leaves, parsley)",
+            "12 pearl onions",
+            "1 cup mushrooms, sliced"
+          ],
+          "method": "Preheat the oven to 350°F. Cook bacon in a Dutch oven until crisp. Remove the bacon and set aside. Add chicken legs to the Dutch oven and brown on all sides. Remove chicken legs and set aside. Add onion and garlic to Dutch oven and cook until softened. Add wine, chicken broth, and tomato paste, and bring to a simmer. Add the chicken legs, bacon, bouquet garni, pearl onions, and mushrooms to the Dutch oven. Cover and cook in the oven for 1 hour or until the chicken is cooked through. Remove the bouquet garni and serve.",
+          "rating": 4.2
+        },
+        {
+          "name": "Salade Niçoise",
+          "ingredients": [
+            "2 cans of tuna, drained",
+            "1 head of lettuce, washed and torn",
+            "4 hard-boiled eggs, peeled and sliced",
+            "1/2 cup green beans, trimmed and cooked",
+            "2 tomatoes, sliced",
+            "1/2 cup Kalamata olives",
+            "2 tbsp red wine vinegar",
+            "1/4 cup olive oil",
+            "1 tbsp Dijon mustard",
+            "Salt and pepper, to taste"
+          ],
+          "method": "In a large bowl, combine the lettuce, tuna, eggs, green beans, tomatoes, and olives. In a small bowl, whisk together the red wine vinegar, olive oil, Dijon mustard, salt, and pepper. Drizzle the dressing over the salad and toss to combine.",
+          "rating": 4.8
+        }
+      ],
+    "likes": 10000
+},
+{
+    "id": 2,
+    "picture": "https://example.com/chef2.jpg",
+    "name": "Alain Ducasse",
+    "experience": 35,
+    "recipes": [
+        {
+          "name": "Soupe de Poissons",
+          "ingredients": [
+            "3 lbs beef chuck roast",
+            "6 slices of bacon",
+            "1 bottle of red wine",
+            "1 onion, chopped",
+            "2 garlic cloves, minced",
+            "3 cups beef broth",
+            "3 carrots, peeled and sliced",
+            "1 bouquet garni (thyme, bay leaves, parsley)"
+          ],
+          "method": "Preheat the oven to 350°F. Cook bacon in a Dutch oven until crisp. Remove the bacon and set aside. Add beef to the Dutch oven and brown on all sides. Remove beef and set aside. Add onion and garlic to Dutch oven and cook until softened. Add wine and beef broth, and bring to a simmer. Add the beef, bacon, carrots, and bouquet garni to the Dutch oven. Cover and cook in the oven for 2-3 hours or until the beef is tender. Remove the bouquet garni and serve.",
+          "rating": 4.5
+        },
+        {
+          "name": "Tournedos Rossini",
+          "ingredients": [
+            "4 chicken legs",
+            "4 slices of bacon",
+            "1 onion, chopped",
+            "2 garlic cloves, minced",
+            "2 cups red wine",
+            "2 cups chicken broth",
+            "2 tbsp tomato paste",
+            "1 bouquet garni (thyme, bay leaves, parsley)",
+            "12 pearl onions",
+            "1 cup mushrooms, sliced"
+          ],
+          "method": "Preheat the oven to 350°F. Cook bacon in a Dutch oven until crisp. Remove the bacon and set aside. Add chicken legs to the Dutch oven and brown on all sides. Remove chicken legs and set aside. Add onion and garlic to Dutch oven and cook until softened. Add wine, chicken broth, and tomato paste, and bring to a simmer. Add the chicken legs, bacon, bouquet garni, pearl onions, and mushrooms to the Dutch oven. Cover and cook in the oven for 1 hour or until the chicken is cooked through. Remove the bouquet garni and serve.",
+          "rating": 4.2
+        },
+        {
+          "name": "Crème Brûlée",
+          "ingredients": [
+            "2 cans of tuna, drained",
+            "1 head of lettuce, washed and torn",
+            "4 hard-boiled eggs, peeled and sliced",
+            "1/2 cup green beans, trimmed and cooked",
+            "2 tomatoes, sliced",
+            "1/2 cup Kalamata olives",
+            "2 tbsp red wine vinegar",
+            "1/4 cup olive oil",
+            "1 tbsp Dijon mustard",
+            "Salt and pepper, to taste"
+          ],
+          "method": "In a large bowl, combine the lettuce, tuna, eggs, green beans, tomatoes, and olives. In a small bowl, whisk together the red wine vinegar, olive oil, Dijon mustard, salt, and pepper. Drizzle the dressing over the salad and toss to combine.",
+          "rating": 4.8
+        }
+      ],
+    "likes": 8000
+},
+{
+    "id": 3,
+    "picture": "https://example.com/chef3.jpg",
+    "name": "Joël Robuchon",
+    "experience": 45,
+    "recipes": [
+        {
+          "name": "Pommes Purée",
+          "ingredients": [
+            "3 lbs beef chuck roast",
+            "6 slices of bacon",
+            "1 bottle of red wine",
+            "1 onion, chopped",
+            "2 garlic cloves, minced",
+            "3 cups beef broth",
+            "3 carrots, peeled and sliced",
+            "1 bouquet garni (thyme, bay leaves, parsley)"
+          ],
+          "method": "Preheat the oven to 350°F. Cook bacon in a Dutch oven until crisp. Remove the bacon and set aside. Add beef to the Dutch oven and brown on all sides. Remove beef and set aside. Add onion and garlic to Dutch oven and cook until softened. Add wine and beef broth, and bring to a simmer. Add the beef, bacon, carrots, and bouquet garni to the Dutch oven. Cover and cook in the oven for 2-3 hours or until the beef is tender. Remove the bouquet garni and serve.",
+          "rating": 4.5
+        },
+        {
+          "name": "Le Caviar",
+          "ingredients": [
+            "4 chicken legs",
+            "4 slices of bacon",
+            "1 onion, chopped",
+            "2 garlic cloves, minced",
+            "2 cups red wine",
+            "2 cups chicken broth",
+            "2 tbsp tomato paste",
+            "1 bouquet garni (thyme, bay leaves, parsley)",
+            "12 pearl onions",
+            "1 cup mushrooms, sliced"
+          ],
+          "method": "Preheat the oven to 350°F. Cook bacon in a Dutch oven until crisp. Remove the bacon and set aside. Add chicken legs to the Dutch oven and brown on all sides. Remove chicken legs and set aside. Add onion and garlic to Dutch oven and cook until softened. Add wine, chicken broth, and tomato paste, and bring to a simmer. Add the chicken legs, bacon, bouquet garni, pearl onions, and mushrooms to the Dutch oven. Cover and cook in the oven for 1 hour or until the chicken is cooked through. Remove the bouquet garni and serve.",
+          "rating": 4.2
+        },
+        {
+          "name": "Le Homard",
+          "ingredients": [
+            "2 cans of tuna, drained",
+            "1 head of lettuce, washed and torn",
+            "4 hard-boiled eggs, peeled and sliced",
+            "1/2 cup green beans, trimmed and cooked",
+            "2 tomatoes, sliced",
+            "1/2 cup Kalamata olives",
+            "2 tbsp red wine vinegar",
+            "1/4 cup olive oil",
+            "1 tbsp Dijon mustard",
+            "Salt and pepper, to taste"
+          ],
+          "method": "In a large bowl, combine the lettuce, tuna, eggs, green beans, tomatoes, and olives. In a small bowl, whisk together the red wine vinegar, olive oil, Dijon mustard, salt, and pepper. Drizzle the dressing over the salad and toss to combine.",
+          "rating": 4.8
+        }
+      ],
+    "likes": 9000
+},
+{
+    "id": 4,
+    "picture": "https://example.com/chef4.jpg",
+    "name": "Anne-Sophie Pic",
+    "experience": 20,
+    "recipes": [
+        {
+          "name": "Le Saumon Fumé",
+          "ingredients": [
+            "3 lbs beef chuck roast",
+            "6 slices of bacon",
+            "1 bottle of red wine",
+            "1 onion, chopped",
+            "2 garlic cloves, minced",
+            "3 cups beef broth",
+            "3 carrots, peeled and sliced",
+            "1 bouquet garni (thyme, bay leaves, parsley)"
+          ],
+          "method": "Preheat the oven to 350°F. Cook bacon in a Dutch oven until crisp. Remove the bacon and set aside. Add beef to the Dutch oven and brown on all sides. Remove beef and set aside. Add onion and garlic to Dutch oven and cook until softened. Add wine and beef broth, and bring to a simmer. Add the beef, bacon, carrots, and bouquet garni to the Dutch oven. Cover and cook in the oven for 2-3 hours or until the beef is tender. Remove the bouquet garni and serve.",
+          "rating": 4.5
+        },
+        {
+          "name": "Le Homard Bleu",
+          "ingredients": [
+            "4 chicken legs",
+            "4 slices of bacon",
+            "1 onion, chopped",
+            "2 garlic cloves, minced",
+            "2 cups red wine",
+            "2 cups chicken broth",
+            "2 tbsp tomato paste",
+            "1 bouquet garni (thyme, bay leaves, parsley)",
+            "12 pearl onions",
+            "1 cup mushrooms, sliced"
+          ],
+          "method": "Preheat the oven to 350°F. Cook bacon in a Dutch oven until crisp. Remove the bacon and set aside. Add chicken legs to the Dutch oven and brown on all sides. Remove chicken legs and set aside. Add onion and garlic to Dutch oven and cook until softened. Add wine, chicken broth, and tomato paste, and bring to a simmer. Add the chicken legs, bacon, bouquet garni, pearl onions, and mushrooms to the Dutch oven. Cover and cook in the oven for 1 hour or until the chicken is cooked through. Remove the bouquet garni and serve.",
+          "rating": 4.2
+        },
+        {
+          "name": "La Pomme de Terre en Croûte de Sel",
+          "ingredients": [
+            "2 cans of tuna, drained",
+            "1 head of lettuce, washed and torn",
+            "4 hard-boiled eggs, peeled and sliced",
+            "1/2 cup green beans, trimmed and cooked",
+            "2 tomatoes, sliced",
+            "1/2 cup Kalamata olives",
+            "2 tbsp red wine vinegar",
+            "1/4 cup olive oil",
+            "1 tbsp Dijon mustard",
+            "Salt and pepper, to taste"
+          ],
+          "method": "In a large bowl, combine the lettuce, tuna, eggs, green beans, tomatoes, and olives. In a small bowl, whisk together the red wine vinegar, olive oil, Dijon mustard, salt, and pepper. Drizzle the dressing over the salad and toss to combine.",
+          "rating": 4.8
+        }
+      ],
+    "likes": 5000
+},
+{
+    "id": 5,
+    "picture": "https://example.com/chef5.jpg",
+    "name": "Yannick Alléno",
+    "experience": 25,
+    "recipes": [
+        {
+          "name": "Les Coquilles Saint-Jacques",
+          "ingredients": [
+            "3 lbs beef chuck roast",
+            "6 slices of bacon",
+            "1 bottle of red wine",
+            "1 onion, chopped",
+            "2 garlic cloves, minced",
+            "3 cups beef broth",
+            "3 carrots, peeled and sliced",
+            "1 bouquet garni (thyme, bay leaves, parsley)"
+          ],
+          "method": "Preheat the oven to 350°F. Cook bacon in a Dutch oven until crisp. Remove the bacon and set aside. Add beef to the Dutch oven and brown on all sides. Remove beef and set aside. Add onion and garlic to Dutch oven and cook until softened. Add wine and beef broth, and bring to a simmer. Add the beef, bacon, carrots, and bouquet garni to the Dutch oven. Cover and cook in the oven for 2-3 hours or until the beef is tender. Remove the bouquet garni and serve.",
+          "rating": 4.5
+        },
+        {
+          "name": "Le Bar de Ligne",
+          "ingredients": [
+            "4 chicken legs",
+            "4 slices of bacon",
+            "1 onion, chopped",
+            "2 garlic cloves, minced",
+            "2 cups red wine",
+            "2 cups chicken broth",
+            "2 tbsp tomato paste",
+            "1 bouquet garni (thyme, bay leaves, parsley)",
+            "12 pearl onions",
+            "1 cup mushrooms, sliced"
+          ],
+          "method": "Preheat the oven to 350°F. Cook bacon in a Dutch oven until crisp. Remove the bacon and set aside. Add chicken legs to the Dutch oven and brown on all sides. Remove chicken legs and set aside. Add onion and garlic to Dutch oven and cook until softened. Add wine, chicken broth, and tomato paste, and bring to a simmer. Add the chicken legs, bacon, bouquet garni, pearl onions, and mushrooms to the Dutch oven. Cover and cook in the oven for 1 hour or until the chicken is cooked through. Remove the bouquet garni and serve.",
+          "rating": 4.2
+        },
+        {
+          "name": "Le Soufflé au Grand Marnier",
+          "ingredients": [
+            "2 cans of tuna, drained",
+            "1 head of lettuce, washed and torn",
+            "4 hard-boiled eggs, peeled and sliced",
+            "1/2 cup green beans, trimmed and cooked",
+            "2 tomatoes, sliced",
+            "1/2 cup Kalamata olives",
+            "2 tbsp red wine vinegar",
+            "1/4 cup olive oil",
+            "1 tbsp Dijon mustard",
+            "Salt and pepper, to taste"
+          ],
+          "method": "In a large bowl, combine the lettuce, tuna, eggs, green beans, tomatoes, and olives. In a small bowl, whisk together the red wine vinegar, olive oil, Dijon mustard, salt, and pepper. Drizzle the dressing over the salad and toss to combine.",
+          "rating": 4.8
+        }
+      ],
+    "likes": 6000
+},
+{
+    "id": 6,
+    "picture": "https://example.com/chef6.jpg",
+    "name": "Éric Ripert",
+    "experience": 30,
+    "recipes": [
+        {
+          "name": "Le Bar Cuit à la Vapeur",
+          "ingredients": [
+            "3 lbs beef chuck roast",
+            "6 slices of bacon",
+            "1 bottle of red wine",
+            "1 onion, chopped",
+            "2 garlic cloves, minced",
+            "3 cups beef broth",
+            "3 carrots, peeled and sliced",
+            "1 bouquet garni (thyme, bay leaves, parsley)"
+          ],
+          "method": "Preheat the oven to 350°F. Cook bacon in a Dutch oven until crisp. Remove the bacon and set aside. Add beef to the Dutch oven and brown on all sides. Remove beef and set aside. Add onion and garlic to Dutch oven and cook until softened. Add wine and beef broth, and bring to a simmer. Add the beef, bacon, carrots, and bouquet garni to the Dutch oven. Cover and cook in the oven for 2-3 hours or until the beef is tender. Remove the bouquet garni and serve.",
+          "rating": 4.5
+        },
+        {
+          "name": "Tournedos Rossini",
+          "ingredients": [
+            "4 chicken legs",
+            "4 slices of bacon",
+            "1 onion, chopped",
+            "2 garlic cloves, minced",
+            "2 cups red wine",
+            "2 cups chicken broth",
+            "2 tbsp tomato paste",
+            "1 bouquet garni (thyme, bay leaves, parsley)",
+            "12 pearl onions",
+            "1 cup mushrooms, sliced"
+          ],
+          "method": "Preheat the oven to 350°F. Cook bacon in a Dutch oven until crisp. Remove the bacon and set aside. Add chicken legs to the Dutch oven and brown on all sides. Remove chicken legs and set aside. Add onion and garlic to Dutch oven and cook until softened. Add wine, chicken broth, and tomato paste, and bring to a simmer. Add the chicken legs, bacon, bouquet garni, pearl onions, and mushrooms to the Dutch oven. Cover and cook in the oven for 1 hour or until the chicken is cooked through. Remove the bouquet garni and serve.",
+          "rating": 4.2
+        },
+        {
+          "name": "Le Soufflé au Fromage",
+          "ingredients": [
+            "2 cans of tuna, drained",
+            "1 head of lettuce, washed and torn",
+            "4 hard-boiled eggs, peeled and sliced",
+            "1/2 cup green beans, trimmed and cooked",
+            "2 tomatoes, sliced",
+            "1/2 cup Kalamata olives",
+            "2 tbsp red wine vinegar",
+            "1/4 cup olive oil",
+            "1 tbsp Dijon mustard",
+            "Salt and pepper, to taste"
+          ],
+          "method": "In a large bowl, combine the lettuce, tuna, eggs, green beans, tomatoes, and olives. In a small bowl, whisk together the red wine vinegar, olive oil, Dijon mustard, salt, and pepper. Drizzle the dressing over the salad and toss to combine.",
+          "rating": 4.8
+        }
+      ],
+    "likes": 7000
+}
+]
